8a5505736c81ff2512ff482ca33db98ed0d49e25
champiww <a24sergiobc@iessanclemente.net>
2024-11-08 09:56:51 +0100

    ej regex

--- ej03/ejRegex/script.js
+++ ej03/ejRegex/script.js
@@ -16,22 +16,56 @@ function verify(regexp, yes, no) {
 //2a
 verify(/ca(?:r|t)/, ['my car', 'bad cats'], ['camper', 'high art']);
 //2b
-verify(/.../, ['pop culture', 'mad props'], ['plop', 'prrrop']);
+verify(/p(?:r)op/, ['pop culture', 'mad props'], ['plop', 'prrrop']);
 //2c
-verify(/.../, ['ferret', 'ferry', 'ferrari'], ['ferrum', 'transfer A']);
+verify(/ferr(et|y|ari)/, ['ferret', 'ferry', 'ferrari'], ['ferrum', 'transfer A']);
 //2d
-verify(/.../, ['how delicious', 'spacious room'], ['ruinous', 'consciousness']);
+verify(/\b\w+ious\b/, ['how delicious', 'spacious room'], ['ruinous', 'consciousness']);
 //2e
-verify(/.../, ['bad punctuation .'], ['escape the period']);
+verify(/\s[.,:;]/, ['bad punctuation .'], ['escape the period']);
 //2f
 verify(
-    /.../,
+    /\b\w{7,}\b/,
     ['Siebentausenddreihundertzweiundzwanzig'],
     ['no', 'three small words']
 );
 //2g
 verify(
-    /.../,
+    /\b[^eE\s]+\b/,
     ['red platypus', 'wobbling nest'],
     ['earth bed', 'learning ape', 'BEET']
 );
+
+//Ej03
+verify(/^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$/, ['01:32:54:67:89:AB'])
+
+//Ej04
+
+function comprobarContrasinal(contrasinal) {
+    // a. Mínimo 8 caracteres
+    if (contrasinal.length < 8) return false;
+    
+    // b. Sen espazos en branco
+    if (/\s/.test(contrasinal)) return false;
+  
+    // c. Comprobar tipos de caracteres
+    let tiposCumpridos = 0;
+    
+    // i. Maiúsculas
+    if (/[A-Z]/.test(contrasinal)) tiposCumpridos++;
+    
+    // ii. Minúsculas
+    if (/[a-z]/.test(contrasinal)) tiposCumpridos++;
+    
+    // iii. Números
+    if (/[0-9]/.test(contrasinal)) tiposCumpridos++;
+    
+    // iv. Caracteres especiais
+    if (/[¡!$?%&#@/\\()=¿?*\[\];,:._<>+\-]/.test(contrasinal)) tiposCumpridos++;
+    
+    // A contrasinal é válida se ten polo menos 3 dos 4 tipos de caracteres
+    return tiposCumpridos >= 3;
+}
+
+console.log(comprobarContrasinal())
+
