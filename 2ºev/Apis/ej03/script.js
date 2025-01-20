142e8847aaa8c97c90a78e173a5f0b5ebe247eca
a24sergiobc <a24sergiobc@iessanclemente.net>
2025-01-20 17:38:43 +0100

    finished ej03

---
+++ 2ºev/Apis/ej03/script.js
@@ -0,0 +1,90 @@
+'use strict';
+
+function rellenarDatos() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((datos) => {
+            rellenarThead();
+            rellenarTbody(datos);
+        });
+}
+
+function rellenarThead() {
+    let tr = document.createElement('tr');
+    let thNombre = document.createElement('th');
+    let thImaxe = document.createElement('th');
+    let thHabilidades = document.createElement('th');
+
+    thNombre.innerText = 'Nombre';
+    thImaxe.innerText = 'Imaxe';
+    thHabilidades.innerText = 'Habiliades';
+
+    tr.append(thNombre);
+    tr.append(thImaxe);
+    tr.append(thHabilidades);
+
+    thead.append(tr);
+}
+
+function rellenarTbody(datos) {
+    let tr = document.createElement('tr');
+    let tdNombre = document.createElement('td');
+    let tdImaxe = document.createElement('td');
+    let tdHabilidades = document.createElement('td');
+
+    tdNombre.innerText = datos.name;
+    let img = document.createElement('img');
+    img.src = datos.sprites.front_default;
+    tdImaxe.append(img);
+    //Lista habilidades
+    let ul = document.createElement('ul');
+
+    datos.abilities.forEach((element) => {
+        let li = document.createElement('li');
+        li.innerText = element.ability.name;
+
+        //Lista url y nombre
+        let ul2 = document.createElement('ul');
+
+        let liUrl = document.createElement('li');
+        let a = document.createElement('a');
+        a.href = element.ability.url;
+        a.innerText = 'Enlace';
+        liUrl.append(a);
+        ul2.append(liUrl);
+
+        let liEsp = document.createElement('li');
+        obtenerDatosSkillEsp(element.ability.url, liEsp);
+        ul2.append(liEsp);
+
+        li.append(ul2);
+        ul.append(li);
+    });
+
+    tdHabilidades.append(ul);
+
+    tr.append(tdNombre);
+    tr.append(tdImaxe);
+    tr.append(tdHabilidades);
+
+    thead.append(tr);
+}
+
+function obtenerDatosSkillEsp(enlace, liEsp, ul2) {
+    let resultado = '';
+    fetch(enlace)
+        .then((response) => response.json())
+        .then((datos) => {
+            liEsp.innerText = datos.names.find(
+                (element) => element.language.name == 'es'
+            ).name;
+        });
+}
+
+let numAleatorio = Math.ceil(Math.random() * 1000);
+
+const url = 'https://pokeapi.co/api/v2/pokemon/' + numAleatorio;
+const thead = document.getElementById('tableHead');
+const tbody = document.getElementById('tableBody');
+
+rellenarDatos();
