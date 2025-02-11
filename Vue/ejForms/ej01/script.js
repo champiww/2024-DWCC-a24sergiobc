543ee1a56001636c1e6430be8a620dd2287f0a02
a24sergiobc <a24sergiobc@iessanclemente.net>
2025-02-11 10:26:10 +0100

    ej02 vue forms

--- Vue/ejForms/ej01/script.js
+++ Vue/ejForms/ej01/script.js
@@ -6,20 +6,20 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 createApp({
     data() {
         return {
-            nome: 'Sergio',
-            year: new Date().getFullYear(),
-            count: 1,
-            src: 'kitten.jpeg',
-            value: 'Sergio',
-            count:0,
+            textbox: '',
+            textarea: '',
+            checkbox: [],
+            radiobuttons: [],
+            select: [],
         };
     },
     methods: {
-        increment() {
-            this.count++;
-        },
-        decrement() {
-            this.count--;
+        reset() {
+            textbox = '';
+            textarea = '';
+            checkbox = [];
+            radiobuttons = [];
+            select = [];
         },
     },
 }).mount('#app');
